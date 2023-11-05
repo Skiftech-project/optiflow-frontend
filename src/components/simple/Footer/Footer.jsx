3406f852fc1de78024dceef1678f98d0d1fab647
ulyagram77 <kirillulianow@gmail.com>
2023-11-05 17:43:38 +0200

    designed footer and header (non-adaptive)

--- src/components/simple/Footer/Footer.jsx
+++ src/components/simple/Footer/Footer.jsx
@@ -1,7 +1,36 @@
 import './Footer.css';
 
 const Footer = () => {
-    return <footer className="footer">Footer</footer>;
+    return (
+        <footer className="footer">
+            <div className="container">
+                <div className="footer__body">
+                    <div className="footer__copyright">
+                        © Optiflow Skiftech-Project
+                    </div>
+                    <nav className="footer__menu">
+                        <ul className="footer__list">
+                            <li className="footer__item">
+                                <a href="#">Головна</a>
+                            </li>
+                            <li className="footer__item">
+                                <a href="#">Калькулятор</a>
+                            </li>
+                            <li className="footer__item">
+                                <a href="#">Візуальний калькулятор</a>
+                            </li>
+                            <li className="footer__item">
+                                <a href="#">Про нас</a>
+                            </li>
+                            <li className="footer__item">
+                                <a href="#">Допомога</a>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </footer>
+    );
 };
 
 export default Footer;
--- src/components/simple/Header/Header.jsx
+++ src/components/simple/Header/Header.jsx
@@ -1,7 +1,46 @@
 import './Header.css';
+import logo from '../../../assets/logo.svg';
 
 const Header = () => {
-    return <header className="header">header</header>;
+    return (
+        <header className="header">
+            <div className="container">
+                <div className="header__body">
+                    <a
+                        href="#"
+                        className="header__logo"
+                    >
+                        <img
+                            src={logo}
+                            alt="logo"
+                        />
+                    </a>
+
+                    <nav className="header__menu menu">
+                        <ul className="menu__list">
+                            <li className="menu__item">
+                                <a href="#">Головна</a>
+                            </li>
+                            <li className="menu__item">
+                                <a href="#">Калькулятор</a>
+                            </li>
+                            <li className="menu__item">
+                                <a href="#">Візуальний калькулятор</a>
+                            </li>
+                            <li className="menu__item">
+                                <a href="#">Про нас</a>
+                            </li>
+                        </ul>
+                    </nav>
+
+                    <div className="header__additional additional">
+                        <div className="additional__lang">UA</div>
+                        <button className="additional__btn">Рєєстрація</button>
+                    </div>
+                </div>
+            </div>
+        </header>
+    );
 };
 
 export default Header;
