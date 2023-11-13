fc209552c4668266ff33d8cb4417a99068581e38
whitegolyb <kirillulianow@gmail.com>
2023-11-13 21:43:28 +0200

    created styled radio button

--- src/components/smart/FirstModule/FirstModule.jsx
+++ src/components/smart/FirstModule/FirstModule.jsx
@@ -1,24 +1,34 @@
-import { Formik, Form, Field } from 'formik';
+import { Formik, Form } from 'formik';
 import axios from 'axios';
 import { useState } from 'react';
 import validationSchema from '../../../core/shemes/ShemaFirstModule';
 
 import InputBlock from '../../simple/InputBlock/InputBlock';
 import { Button } from 'antd';
+import { Radio } from 'antd';
+import { Select } from 'antd';
+import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
 import './FirstModule.css';
 
 const initialValues = {
-    distance: '',
+    distance: 1,
     sensitivity: '',
     power: '',
     angleWidth: 1,
     angleHeight: 1,
     spotWidth: 1,
     spotHeight: 1,
+    plumeForm: 'rectangle',
 };
 
 const FirstModule = () => {
     const [selectedOption, setSelectedOption] = useState('angles');
+    // const [data, setData] = useState({
+    //     max_area: 0,
+    //     max_distance: 0,
+    //     angle_width: 0,
+    //     angle_height: 0,
+    // });
 
     const handleOptionChange = (e) => {
         setSelectedOption(e.target.value);
@@ -30,6 +40,7 @@ const FirstModule = () => {
             if (selectedOption === 'angles') {
                 delete updatedValues.spotWidth;
                 delete updatedValues.spotHeight;
+                delete updatedValues.distance;
             } else if (selectedOption === 'dimensions') {
                 delete updatedValues.angleWidth;
                 delete updatedValues.angleHeight;
@@ -45,7 +56,7 @@ const FirstModule = () => {
             );
 
             console.log('Успешно отправлено', response.data);
-            setData(response.data);
+            //setData(response.data);
         } catch (error) {
             console.error('Произошла ошибка при отправке данных', error);
         }
@@ -79,123 +90,103 @@ const FirstModule = () => {
                             />
                         </div>
 
-                        {/* form radio */}
-                        <div className="form__item">
-                            <label className="form__label">
-                                Оберіть опцію:
-                            </label>
-                            <div className="radio">
-                                <div className="radio__item">
-                                    <label>Кути:</label>
-                                    <Field
-                                        type="radio"
-                                        name="option"
-                                        value="angles"
-                                        checked={selectedOption === 'angles'}
-                                        onChange={handleOptionChange}
-                                    />
-                                </div>
-                                <div className="radio__item">
-                                    <label>Пляма:</label>
-                                    <Field
-                                        type="radio"
-                                        name="option"
-                                        value="dimensions"
-                                        checked={
-                                            selectedOption === 'dimensions'
-                                        }
-                                        onChange={handleOptionChange}
-                                    />
-                                </div>
+                        <div className="radio">
+                            <div className="radio__label">
+                                Оберіть опцію розрахунку:
                             </div>
-                        </div>
+                            <Radio.Group
+                                defaultValue="angles"
+                                buttonStyle="solid"
+                                size="middle"
+                                className="radio__group"
+                            >
+                                <Radio.Button
+                                    className="radio__item"
+                                    type="radio"
+                                    name="option"
+                                    value="angles"
+                                    checked={selectedOption === 'angles'}
+                                    onChange={handleOptionChange}
+                                >
+                                    Кути
+                                </Radio.Button>
+                                <Radio.Button
+                                    className="radio__item"
+                                    type="radio"
+                                    name="option"
+                                    value="dimensions"
+                                    checked={selectedOption === 'dimensions'}
+                                    onChange={handleOptionChange}
+                                >
+                                    Пляма
+                                </Radio.Button>
+                            </Radio.Group>
 
-                        {selectedOption === 'angles' && (
-                            <>
-                                <div className="form__item">
-                                    <InputBlock
-                                        label="Кут Ширини (°):"
-                                        id="angleWidth"
-                                        name="angleWidth"
-                                        type="number"
-                                    />
-                                </div>
-                                <div className="form__item">
-                                    <InputBlock
-                                        label="Кут Висоти (°):"
-                                        id="angleHeight"
-                                        name="angleHeight"
-                                        type="number"
-                                    />
-                                </div>
-                            </>
-                        )}
+                            {selectedOption === 'angles' && (
+                                <>
+                                    <div className="form__item">
+                                        <InputBlock
+                                            label="Кут Ширини (°):"
+                                            id="angleWidth"
+                                            name="angleWidth"
+                                            type="number"
+                                        />
+                                    </div>
+                                    <div className="form__item">
+                                        <InputBlock
+                                            label="Кут Висоти (°):"
+                                            id="angleHeight"
+                                            name="angleHeight"
+                                            type="number"
+                                        />
+                                    </div>
+                                </>
+                            )}
 
-                        {selectedOption === 'dimensions' && (
-                            <>
-                                <div className="form__item">
-                                    <InputBlock
-                                        label="Відстань (м):"
-                                        id="distance"
-                                        name="distance"
-                                        type="number"
-                                    />
-                                </div>
-                                <div className="form__item">
-                                    <InputBlock
-                                        label="Ширина Плями (м):"
-                                        id="spotWidth"
-                                        name="spotWidth"
-                                        type="number"
-                                    />
-                                </div>
-                                <div className="form__item">
-                                    <InputBlock
-                                        label="Висота Плями (м):"
-                                        id="spotHeight"
-                                        name="spotHeight"
-                                        type="number"
-                                    />
-                                </div>
-                            </>
-                        )}
-                    </Form>
-                </Formik>
-            </div>
-            <div className="form">
-                <div className="form__title">модуль 2</div>
-                <Formik
-                    initialValues={initialValues}
-                    validationSchema={validationSchema}
-                    onSubmit={handleFormSubmit}
-                >
-                    <Form className="form__body">
-                        <div className="form__item">
-                            <InputBlock
-                                label="Кут Ширини (°):"
-                                id="angleWidth"
-                                name="angleWidth"
-                                type="number"
-                            />
+                            {selectedOption === 'dimensions' && (
+                                <>
+                                    <div className="form__item">
+                                        <InputBlock
+                                            label="Відстань (м):"
+                                            id="distance"
+                                            name="distance"
+                                            type="number"
+                                        />
+                                    </div>
+                                    <div className="form__item">
+                                        <InputBlock
+                                            label="Ширина Плями (м):"
+                                            id="spotWidth"
+                                            name="spotWidth"
+                                            type="number"
+                                        />
+                                    </div>
+                                    <div className="form__item">
+                                        <InputBlock
+                                            label="Висота Плями (м):"
+                                            id="spotHeight"
+                                            name="spotHeight"
+                                            type="number"
+                                        />
+                                    </div>
+                                </>
+                            )}
                         </div>
-                        <div className="form__item">
-                            <InputBlock
-                                label="Кут Висоти (°):"
-                                id="angleHeight"
-                                name="angleHeight"
-                                type="number"
-                            />
-                        </div>
-
-                        <div className="form__item">
-                            <InputBlock
-                                label="Мінімальний розмір плями (м):"
-                                id="minSize"
-                                name="minSize"
-                                type="number"
-                            />
-                        </div>
-
+                        <Select
+                            name="plumeForm"
+                            className="form__select"
+                            defaultValue="rectangle"
+                            options={[
+                                {
+                                    value: 'rectangle',
+                                    label: 'Прямокутник',
+                                },
+                                {
+                                    value: 'ellipse',
+                                    label: 'Еліпс',
+                                },
+                            ]}
+                        />
                         <Button
                             className="form__button"
                             type="primary"
@@ -203,42 +194,6 @@ const FirstModule = () => {
                         >
                             Розрахувати
                         </Button>
-                    </Form>
-                </Formik>
-            </div>
-            <div className="form">
-                <div className="form__title">модуль 3</div>
-                <Formik
-                    initialValues={initialValues}
-                    validationSchema={validationSchema}
-                    onSubmit={handleFormSubmit}
-                >
-                    <Form className="form__body">
-                        <div className="form__item">
-                            <InputBlock
-                                label="Кут Ширини (°):"
-                                id="angleWidth"
-                                name="angleWidth"
-                                type="number"
-                            />
-                        </div>
-                        <div className="form__item">
-                            <InputBlock
-                                label="Кут Висоти (°):"
-                                id="angleHeight"
-                                name="angleHeight"
-                                type="number"
-                            />
-                        </div>
-
-                        <div className="form__item">
-                            <InputBlock
-                                label="Дистанція (м):"
-                                id="power"
-                                name="power"
-                                type="number"
-                            />
-                        </div>
                     </Form>
                 </Formik>
             </div>
