efd41cf7fe54aca490b3c96b0703fd5a2ba2a49f
Whitegolyb <kirillulianow@gmail.com>
2024-02-19 23:55:54 +0200

    added MainPage and its route

--- src/components/pages/index.js
+++ src/components/pages/index.js
@@ -1,5 +1,6 @@
 import CalculatorPage from './CalculatorPage';
 import NotFoundPage from './NotFoundPage';
 import RayPage from './RayPage';
+import MainPage from './MainPage';
 
-export { CalculatorPage, NotFoundPage, RayPage };
+export { CalculatorPage, NotFoundPage, RayPage, MainPage };
