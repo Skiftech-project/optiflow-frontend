4957849736949300218ae34df3d0b32228301c94
Whitegolyb <kirillulianow@gmail.com>
2024-03-02 15:48:53 +0200

    added link components

--- src/components/ui/index.js
+++ src/components/ui/index.js
@@ -4,5 +4,7 @@ import Button from './Button/DefaultButton';
 import Preloader from './Preloader/Preloader';
 import Input from './Input/Input';
 import Select from './Select/Select';
+import Link from './Link/Link';
+import NavLink from './Link/NavLink';
 
-export { TitleBlock, Spinner, Button, Preloader, Input, Select };
+export { TitleBlock, Spinner, Button, Preloader, Input, Select, Link, NavLink };
