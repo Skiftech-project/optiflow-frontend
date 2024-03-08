6b10d74ad6d181ce952548c1b6f48d4c1859de72
whitegolyb <kirillulianow@gmail.com>
2024-03-08 21:58:46 +0200

    refactor: it became reusable and adaptive

--- src/components/ui/TitleBlock/TitleBlock.jsx
+++ src/components/ui/TitleBlock/TitleBlock.jsx
@@ -1,34 +1,47 @@
-import Typography from '@mui/material/Typography';
+import { Typography } from '@mui/material';
 import PropTypes from 'prop-types';
 
-const style = {
+const defaultStyle = {
+    color: '#1E55B3',
+    textTransform: 'uppercase',
+    fontWeight: 'bold',
+    fontSize: { xs: 14, sm: 18, md: 21 },
+};
+
+const blockStyle = {
     padding: '13px 0',
-    textAlign: 'center',
     borderRadius: '10px',
     background: '#FFF',
     boxShadow: '1px 1px 4px 0px rgba(0, 0, 0, 0.25)',
 };
 
-const TitleBlock = props => {
+const TitleBlock = ({
+    align = 'center',
+    component = 'h2',
+    block,
+    sx,
+    children,
+    ...props
+}) => {
+    const style = block ? { ...defaultStyle, ...blockStyle } : { ...defaultStyle };
     return (
-        <div style={style}>
-            <Typography
-                sx={{
-                    color: '#1E55B3',
-                    textTransform: 'uppercase',
-                    fontWeight: 'bold',
-                    fontSize: 21,
-                }}
-                component="h2"
-            >
-                {props.title}
-            </Typography>
-        </div>
+        <Typography
+            align={align}
+            sx={{ ...style, ...sx }}
+            component={component}
+            {...props}
+        >
+            {children}
+        </Typography>
     );
 };
 
 TitleBlock.propTypes = {
-    title: PropTypes.string.isRequired,
+    align: PropTypes.oneOf(['center', 'left', 'right']),
+    block: PropTypes.bool,
+    component: PropTypes.elementType,
+    sx: PropTypes.object,
+    children: PropTypes.node.isRequired,
 };
 
 export default TitleBlock;
