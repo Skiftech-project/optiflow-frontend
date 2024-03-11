6b43fd0ad4655388c0f48d2b973910a0a8fa0443
whitegolyb <kirillulianow@gmail.com>
2024-03-11 15:32:00 +0200

    added form controlls and provider for form fields

--- src/components/pages/CalculatorPage.jsx
+++ src/components/pages/CalculatorPage.jsx
@@ -1,14 +1,39 @@
+import { FormProvider, useForm } from 'react-hook-form';
 import { Box } from '@mui/material';
-import { TitleBlock } from '../ui';
+import { TitleBlock, Button } from '../ui';
 import { FormBlock } from '../interface';
 
 const CalculatorPage = () => {
+    const methods = useForm({
+        defaultValues: {
+            plumeForm: 'ellipse',
+            distance: '',
+            spotHeight: '',
+            spotWidth: '',
+            angleWidth: '',
+            angleHeight: '',
+            sensitivity: '',
+            power: '',
+            minPlumeSize: '',
+            distanceModuleThird: '',
+        },
+    });
+
+    const handleSubmit = data => {
+        console.log(JSON.stringify(data, null, 2));
+    };
+
     return (
-        <Box sx={{ marginTop: '70px' }}>
-            <TitleBlock block>Калькулятор розсіяння зони випромінювання</TitleBlock>
-            <FormBlock />
-            <TitleBlock block>Результати обчислень</TitleBlock>
-        </Box>
+        <FormProvider {...methods}>
+            <Box sx={{ marginTop: '70px' }}>
+                <TitleBlock block>Калькулятор розсіяння зони випромінювання</TitleBlock>
+                <FormBlock />
+                <TitleBlock block>Результати обчислень</TitleBlock>
+                <Button onClick={methods.handleSubmit(handleSubmit)} color="primary">
+                    submit
+                </Button>
+            </Box>
+        </FormProvider>
     );
 };
 
