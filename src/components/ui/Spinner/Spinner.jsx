f303b94c5079b5be18d4ac1963edd92f25f5f733
ulyagram77 <kirillulianow@gmail.com>
2024-03-12 21:20:05 +0200

    updated spinner styles

--- src/components/ui/Spinner/Spinner.jsx
+++ src/components/ui/Spinner/Spinner.jsx
@@ -1,7 +1,19 @@
 import { CircleLoader } from 'react-spinners';
+import { useTheme } from '@mui/material/styles';
+
+const style = {
+    position: 'absolute',
+    left: '50%',
+    top: '50%',
+    transform: 'translate(-50%, -50%)',
+};
 
 const Spinner = () => {
-    return <CircleLoader />;
+    const theme = useTheme();
+
+    return (
+        <CircleLoader size={100} color={theme.palette.primary.dark} cssOverride={style} />
+    );
 };
 
 export default Spinner;
