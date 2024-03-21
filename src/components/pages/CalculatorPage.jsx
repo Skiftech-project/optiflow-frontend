57eb893b10f9475d8312a07d30a177aa5af07f1d
Whitegolyb <kirillulianow@gmail.com>
2024-03-21 15:50:11 +0200

    created prototype of summarizing data in md

--- src/components/pages/CalculatorPage.jsx
+++ src/components/pages/CalculatorPage.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { yupResolver } from '@hookform/resolvers/yup';
 import { FormProvider, useForm } from 'react-hook-form';
 import { Stack } from '@mui/material';
@@ -5,8 +6,12 @@ import { Stack } from '@mui/material';
 import { TitleBlock, Button, Block } from '../ui';
 import { FormBlock } from '../interface';
 import { validationSchemaCalc } from 'src/core/shemes';
+import Markdown from 'react-markdown';
+import remarkGfm from 'remark-gfm';
 
 const CalculatorPage = () => {
+    const [data, setData] = useState({});
+
     const methods = useForm({
         defaultValues: {
             plumeForm: 'ellipse',
@@ -26,7 +31,26 @@ const CalculatorPage = () => {
 
     const handleSubmit = data => {
         console.log(JSON.stringify(data, null, 2));
+        setData(data);
     };
+
+    const markdownData = `
+
+Нижче приведені розрахунки у форматі **md**:
+
+| Параметр | Значення   |
+| ----- | ------- |
+| distanceModuleThird  | ${data.distanceModuleThird || '-'} |
+| minPlumeSize | ${data.minPlumeSize || '-'} |
+| power | ${data.power || '-'} |
+| sensitivity | ${data.sensitivity || '-'} |
+| angleHeight | ${data.angleHeight || '-'} |
+| angleWidth | ${data.angleWidth || '-'} |
+| spotWidth | ${data.spotWidth || '-'} |
+| spotHeight | ${data.spotHeight || '-'} |
+| distance | ${data.distance || '-'} |
+| plumeForm | ${data.plumeForm || '-'} |
+`;
 
     return (
         <FormProvider {...methods}>
@@ -36,6 +60,8 @@ const CalculatorPage = () => {
                 <TitleBlock block>Результати обчислень</TitleBlock>
 
                 <Block padding="30px">
+                    <Markdown remarkPlugins={[remarkGfm]}>{markdownData}</Markdown>
+
                     <Button
                         disabled={!methods.formState.isValid}
                         onClick={methods.handleSubmit(handleSubmit)}
