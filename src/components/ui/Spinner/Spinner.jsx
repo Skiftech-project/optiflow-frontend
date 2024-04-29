bad9a2af5b2be3871c11942d93037a9048b3db1c
whitegolyb <kirillulianow@gmail.com>
2024-04-29 11:47:04 +0300

    changed spinner view

--- src/components/ui/Spinner/Spinner.jsx
+++ src/components/ui/Spinner/Spinner.jsx
@@ -1,18 +1,15 @@
-import { CircleLoader } from 'react-spinners';
-import { useTheme } from '@mui/material/styles';
-
-const style = {
-    position: 'absolute',
-    left: '50%',
-    top: '50%',
-    transform: 'translate(-50%, -50%)',
-};
+import './Spinner.css';
 
 const Spinner = () => {
-    const theme = useTheme();
-
     return (
-        <CircleLoader size={100} color={theme.palette.primary.dark} cssOverride={style} />
+        <div className="spinner">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
     );
 };
 
