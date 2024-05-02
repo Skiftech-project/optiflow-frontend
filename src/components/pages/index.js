c6f76010ef3ca4fe45c914a17dc87e6556a06a6a
whitegolyb <kirillulianow@gmail.com>
2024-05-02 16:29:08 +0300

    designed 404 page

--- src/components/pages/index.js
+++ src/components/pages/index.js
@@ -1,5 +1,5 @@
 import CalculatorPage from './CalculatorPage';
-import NotFoundPage from './NotFoundPage';
+import NotFoundPage from './NotFoundPage/NotFoundPage';
 import RayPage from './RayPage';
 import MainPage from './MainPage';
 import SignInPage from './SignInPage';
