5d8448cc767d07d0f750cb06fb65c0894b0bdc4a
whitegolyb <kirillulianow@gmail.com>
2024-05-02 18:26:46 +0300

    changed spinner to preloader for pages

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,18 @@ import { Suspense } from 'react';
 
 import { BrowserRouter as Router } from 'react-router-dom';
 import { ThemeProvider } from '@mui/material/styles';
-import { Container } from '@mui/material';
 
 import AppRoutes from './routes';
-import { Header } from './components/interface';
-import { Spinner } from './components/ui';
+import { Preloader } from './components/ui';
 import { lightTheme } from './core/themes';
 
 const App = () => {
     return (
         <ThemeProvider theme={lightTheme}>
             <Router>
-                <Header />
-                <Container maxWidth="xl" component="main">
-                    <Suspense fallback={<Spinner />}>
-                        <AppRoutes />
-                    </Suspense>
-                </Container>
+                <Suspense fallback={<Preloader />}>
+                    <AppRoutes />
+                </Suspense>
             </Router>
         </ThemeProvider>
     );
