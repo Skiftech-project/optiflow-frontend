56aa4ca65c61356164acce0e8cf80088bce703aa
VladSurkov <surkov.vladyslav.34@gmail.com>
2024-05-06 19:19:37 +0300

    changed sing in and sign up pages

--- src/components/pages/SignInPage.jsx
+++ src/components/pages/SignInPage.jsx
@@ -1,6 +1,6 @@
-import { Block, TitleBlock, Input, Button } from 'src/components/ui';
+import { TitleBlock, Input, Button } from 'src/components/ui';
 import { FormProvider, useForm } from 'react-hook-form';
-import { Box } from '@mui/material';
+import { Box, Grid } from '@mui/material';
 import { Link } from 'react-router-dom';
 import { useAuthService } from 'src/core/services';
 import { useSelector } from 'react-redux';
@@ -8,7 +8,10 @@ import { useSelector } from 'react-redux';
 import { yupResolver } from '@hookform/resolvers/yup';
 import { validationShemaLogin } from 'src/core/shemes';
 
-import { FormStyle } from 'src/styles';
+import Paper from '@mui/material/Paper';
+import Avatar from '@mui/material/Avatar';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import { Copyright } from '@mui/icons-material';
 
 const SignInPage = () => {
     const { login } = useAuthService();
@@ -30,47 +33,101 @@ const SignInPage = () => {
 
     return (
         <FormProvider {...methods}>
-            <form>
-                <Block sx={FormStyle}>
+            <Grid container component="main" sx={{ height: '100vh' }}>
+                <Grid
+                    item
+                    xs={false}
+                    sm={4}
+                    md={7}
+                    sx={{
+                        backgroundImage:
+                            'url(https://source.unsplash.com/random?wallpapers)',
+                        backgroundRepeat: 'no-repeat',
+                        backgroundColor: t =>
+                            t.palette.mode === 'light'
+                                ? t.palette.grey[50]
+                                : t.palette.grey[900],
+                        backgroundSize: 'cover',
+                        backgroundPosition: 'center',
+                    }}
+                />
+
+                <Grid
+                    item
+                    xs={12}
+                    sm={8}
+                    md={5}
+                    component={Paper}
+                    elevation={6}
+                    square
+                    sx={{ padding: '30px' }}
+                    alignContent="center"
+                >
+                    <Avatar
+                        sx={{
+                            m: 1,
+                            bgcolor: 'secondary.main',
+                            left: '50%', // TODO: change code
+                            transform: 'translate(-70%, 0)', // TODO: change code
+                        }}
+                    >
+                        <LockOutlinedIcon />
+                    </Avatar>
+
                     <TitleBlock
                         textAlign="center"
-                        sx={{ textTransform: 'normal', marginBottom: '15px' }}
+                        sx={{
+                            textTransform: 'normal',
+                            marginBottom: '15px',
+                            fontSize: '25px',
+                        }}
                     >
                         Увійти
                     </TitleBlock>
 
-                    <Input
-                        name="email"
-                        id="email"
-                        fullWidth
-                        label="Пошта користувача:"
-                        sx={{ marginBottom: '15px' }}
-                        type="email"
-                    />
+                    <Box
+                        component="form"
+                        noValidate
+                        onSubmit={handleSubmit}
+                        sx={{ mt: 1 }}
+                    >
+                        <Input
+                            name="email"
+                            id="email"
+                            fullWidth
+                            label="Пошта користувача:"
+                            sx={{ marginBottom: '15px' }}
+                            type="email"
+                            size="medium"
+                        />
 
-                    <Input
-                        name="password"
-                        id="password"
-                        fullWidth
-                        label="Пароль користувача:"
-                        sx={{ marginBottom: '15px' }}
-                        type="password"
-                    />
+                        <Input
+                            name="password"
+                            id="password"
+                            fullWidth
+                            label="Пароль користувача:"
+                            sx={{ marginBottom: '15px' }}
+                            type="password"
+                            size="medium"
+                        />
 
-                    <Box sx={{ display: 'flex', alignItems: 'center', gap: '20px' }}>
-                        <Button
-                            disabled={!methods.formState.isValid}
-                            onClick={methods.handleSubmit(handleSubmit)}
-                            color="primary"
-                            loading={dataLoadingStatus === 'loading'}
-                        >
-                            Увійти
-                        </Button>
+                        <Box sx={{ display: 'flex', alignItems: 'center', gap: '20px' }}>
+                            <Button
+                                disabled={!methods.formState.isValid}
+                                onClick={methods.handleSubmit(handleSubmit)}
+                                color="primary"
+                                loading={dataLoadingStatus === 'loading'}
+                            >
+                                Увійти
+                            </Button>
 
-                        <Link to="/register">Створити акаунт</Link>
+                            <Link to="/register">Створити акаунт</Link>
+                        </Box>
+
+                        <Copyright sx={{ mt: 5 }} />
                     </Box>
-                </Block>
-            </form>
+                </Grid>
+            </Grid>
         </FormProvider>
     );
 };
--- src/components/pages/SignUpPage.jsx
+++ src/components/pages/SignUpPage.jsx
@@ -1,13 +1,13 @@
-import { Block, TitleBlock, Input, Button } from 'src/components/ui';
+import { TitleBlock, Input, Button } from 'src/components/ui';
 import { FormProvider, useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
-import { Box } from '@mui/material';
+import { Box, Grid, Paper, Avatar } from '@mui/material';
 import { Link } from 'react-router-dom';
 import { useAuthService } from 'src/core/services';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 
 import { validationShemaRegistration } from 'src/core/shemes';
 
-import { FormStyle } from 'src/styles';
 import { useSelector } from 'react-redux';
 
 const SignUpPage = () => {
@@ -31,55 +31,107 @@ const SignUpPage = () => {
 
     return (
         <FormProvider {...methods}>
-            <form>
-                <Block sx={FormStyle}>
+            <Grid container component="main" sx={{ height: '100vh' }}>
+                <Grid
+                    item
+                    xs={12}
+                    sm={8}
+                    md={5}
+                    component={Paper}
+                    elevation={6}
+                    square
+                    sx={{ padding: '30px' }}
+                    alignContent="center"
+                >
+                    <Avatar
+                        sx={{
+                            m: 1,
+                            bgcolor: 'secondary.main',
+                            left: '50%', // TODO: change code
+                            transform: 'translate(-70%, 0)', // TODO: change code
+                        }}
+                    >
+                        <LockOutlinedIcon />
+                    </Avatar>
+
                     <TitleBlock
                         textAlign="center"
-                        sx={{ textTransform: 'normal', marginBottom: '15px' }}
+                        sx={{
+                            textTransform: 'normal',
+                            marginBottom: '15px',
+                            fontSize: '25px',
+                        }}
                     >
                         Зареєструватися
                     </TitleBlock>
 
-                    <Input
-                        name="username"
-                        id="username"
-                        fullWidth
-                        label="Ім'я користувача:"
-                        sx={{ marginBottom: '15px' }}
-                    />
+                    <Box
+                        component="form"
+                        noValidate
+                        onSubmit={handleSubmit}
+                        sx={{ mt: 1 }}
+                    >
+                        <Input
+                            name="username"
+                            id="username"
+                            fullWidth
+                            label="Ім'я користувача:"
+                            sx={{ marginBottom: '15px' }}
+                            size="medium"
+                        />
 
-                    <Input
-                        name="email"
-                        id="email"
-                        fullWidth
-                        label="Пошта користувача:"
-                        sx={{ marginBottom: '15px' }}
-                        type="email"
-                    />
+                        <Input
+                            name="email"
+                            id="email"
+                            fullWidth
+                            label="Пошта користувача:"
+                            sx={{ marginBottom: '15px' }}
+                            type="email"
+                            size="medium"
+                        />
 
-                    <Input
-                        name="password"
-                        id="password"
-                        fullWidth
-                        label="Пароль користувача:"
-                        sx={{ marginBottom: '15px' }}
-                        type="password"
-                    />
+                        <Input
+                            name="password"
+                            id="password"
+                            fullWidth
+                            label="Пароль користувача:"
+                            sx={{ marginBottom: '15px' }}
+                            type="password"
+                            size="medium"
+                        />
 
-                    <Box sx={{ display: 'flex', alignItems: 'center', gap: '20px' }}>
-                        <Button
-                            disabled={!methods.formState.isValid}
-                            onClick={methods.handleSubmit(handleSubmit)}
-                            color="primary"
-                            loading={dataLoadingStatus === 'loading'}
-                        >
-                            Зареєструватися
-                        </Button>
-
-                        <Link to="/login">Акаунт вже існує?</Link>
+                        <Box sx={{ display: 'flex', alignItems: 'center', gap: '20px' }}>
+                            <Button
+                                disabled={!methods.formState.isValid}
+                                onClick={methods.handleSubmit(handleSubmit)}
+                                color="primary"
+                                loading={dataLoadingStatus === 'loading'}
+                            >
+                                Зареєструватися
+                            </Button>
+                            <Link to="/login">Акаунт вже існує?</Link>
+                        </Box>
                     </Box>
-                </Block>
-            </form>
+                </Grid>
+
+                <Grid
+                    item
+                    xs={false}
+                    sm={4}
+                    md={7}
+                    sx={{
+                        backgroundImage:
+                            'url(https://source.unsplash.com/random?wallpapers)',
+                        backgroundRepeat: 'no-repeat',
+                        backgroundColor: t =>
+                            t.palette.mode === 'light'
+                                ? t.palette.grey[50]
+                                : t.palette.grey[900],
+                        backgroundSize: 'cover',
+                        backgroundPosition: 'center',
+                    }}
+                />
+            </Grid>
         </FormProvider>
     );
 };
