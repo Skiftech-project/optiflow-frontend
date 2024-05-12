aa7d7864dba8f6f7c3f8e5d52dc5af66c70e1ec6
kyrylo <kirillulianow@gmail.com>
2024-05-12 15:23:30 +0300

    fix: changed layount to adapative grid

--- src/components/pages/NotFoundPage/NotFoundPage.jsx
+++ src/components/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,17 +1,18 @@
+import { Grid } from '@mui/material';
+
 import { Button } from 'src/components/ui';
 
 import './NotFoundPage.css';
 
 const btnStyles = {
-    width: { xs: '80%', sm: 'fit-content' },
     padding: '20px 25px',
     fontSize: { sm: '1rem', lg: '1.2rem' },
-    position: 'absolute',
-    top: '700px',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    zIndex: 5,
-    bottom: 0,
+    // position: 'absolute',
+    // top: '700px',
+    // left: '50%',
+    // transform: 'translate(-50%, -50%)',
+    // zIndex: 5,
+    // bottom: 0,
 };
 
 const NotFoundPage = () => {
@@ -30,22 +31,32 @@ const NotFoundPage = () => {
                 </div>
             </div>
 
-            <section className="error">
-                <div className="error__message message">
-                    <h2 className="message__title">404 Not Found</h2>
-                    <h3 className="message_subtitle">
-                        На жаль ,сторінку, яку ви шукали, не знайдено
-                    </h3>
-                    <p className="message__text">
-                        Посилання, за яким ви перейшли, може бути непрацюючим або більше
-                        не існує. Будь-ласка, спробуйте ще раз або перейдіть на головну
-                        сторінку.
-                    </p>
-                </div>
-            </section>
-            <Button className="btn" link to="/" sx={btnStyles}>
-                Повернутися до головної
-            </Button>
+            <Grid
+                container
+                spacing={0}
+                gap={5}
+                direction="column"
+                alignItems="center"
+                justifyContent="center"
+                sx={{ minHeight: '100vh' }}
+            >
+                <section className="error">
+                    <div className="message">
+                        <h2 className="message__title">404 Not Found</h2>
+                        <h3 className="message_subtitle">
+                            На жаль ,сторінку, яку ви шукали, не знайдено
+                        </h3>
+                        <p className="message__text">
+                            Посилання, за яким ви перейшли, може бути непрацюючим або
+                            більше не існує. Будь-ласка, спробуйте ще раз або перейдіть на
+                            головну сторінку.
+                        </p>
+                    </div>
+                </section>
+                <Button link to="/" sx={btnStyles}>
+                    Повернутися до головної
+                </Button>
+            </Grid>
         </div>
     );
 };
