3928feeb611d1076d4a0652df5654611e036d244
kyrylo <kirillulianow@gmail.com>
2024-05-13 01:55:36 +0300

    added alias for fonts folder

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
 import legacy from '@vitejs/plugin-legacy';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -16,6 +16,7 @@ export default defineConfig({
             assets: '/src/assets',
             components: '/src/components',
             core: '/src/core',
+            fonts: '/src/fonts',
             styles: '/src/styles',
             routes: '/src/routes',
         },
