d6f6299a9b843bc21492d5c7659f1858d9381de9
kyrylo <kirillulianow@gmail.com>
2024-05-13 02:34:05 +0300

    added reset baseline

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Suspense } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 
+import { CssBaseline } from '@mui/material';
 import { ThemeProvider } from '@mui/material/styles';
 
 import { Preloader } from './components/ui';
@@ -11,6 +12,7 @@ import AppRoutes from './routes';
 const App = () => {
     return (
         <ThemeProvider theme={lightTheme}>
+            <CssBaseline />
             <AuthProvider>
                 <Router>
                     <Suspense fallback={<Preloader />}>
