e44b43b2571813a3c8ce2ed0ad6989c41c91212b
kyrylo <kirillulianow@gmail.com>
2024-05-15 12:01:30 +0300

    added assets for main page

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,5 +1,8 @@
+import calcAdv from './advantages/calculator.jpeg';
+import diagramAdv from './advantages/diagram.jpg';
+import rayAdv from './advantages/ray.jpg';
+import skiftech from './companies/skiftech.jpg';
 import logo from './logo.svg';
-import pattern from './pattern.png';
-import skiftech from './skiftech.jpg';
+import pattern from './others/pattern.png';
 
-export { logo, pattern, skiftech };
+export { logo, pattern, skiftech, calcAdv, diagramAdv, rayAdv };
