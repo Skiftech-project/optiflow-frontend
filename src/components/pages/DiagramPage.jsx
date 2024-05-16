e220dbe94928540dc53c800619f1413d0e358150
Whitegolyb <kirillulianow@gmail.com>
2024-05-16 16:35:13 +0300

    initialized diagram page

---
+++ src/components/pages/DiagramPage.jsx
@@ -0,0 +1,11 @@
+import { Header } from '../interface';
+
+const DiagramPage = () => {
+    return (
+        <>
+            <Header /> <div>Diagram 3d</div>
+        </>
+    );
+};
+
+export default DiagramPage;
