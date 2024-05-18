0317e7e66a643e89626062ef133f9ea0bb1bab35
kyrylo <kirillulianow@gmail.com>
2024-05-18 19:17:12 +0300

    implemented cashed global state storage

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,18 @@
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 
+import { PersistGate } from 'redux-persist/integration/react';
+
 import { AppContainer } from './components/interface';
-import store from './core/store';
+import { Preloader } from './components/ui';
+import store, { persistor } from './core/store';
 import './styles/index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <Provider store={store}>
-        <AppContainer />
+        <PersistGate loading={<Preloader />} persistor={persistor}>
+            <AppContainer />
+        </PersistGate>
     </Provider>,
 );
