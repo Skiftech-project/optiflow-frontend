32e687ac2faa2874711a0e12ca925548da29c387
kyrylo <kirillulianow@gmail.com>
2024-05-21 13:12:14 +0300

    added comments

--- src/core/config/endpoints.js
+++ src/core/config/endpoints.js
@@ -1,13 +1,17 @@
+//base url
 // export const apiBase = 'http://127.0.0.1:5000';
 export const apiBase = 'https://optiflowbackend.azurewebsites.net';
 
+//auth endpoints
 export const registerUrl = apiBase + '/auth/register';
 export const loginUrl = apiBase + '/auth/login';
 export const logoutUrl = apiBase + '/auth/logout';
 export const refreshTokenUrl = apiBase + '/auth/refresh';
 
+//calculations endpoints
 export const calculationsUrl = apiBase + '/2d';
 
+//user endpoints
 export const updateUserDataUrl = apiBase + '/auth/updateProfile';
 export const updatePasswordUrl = apiBase + '/auth/updateUserPassword';
 export const forgotPasswordUrl = apiBase + '/auth/sendResetEmail';
