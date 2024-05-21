9005637df2df2ba8e5667816dc52c7a8ef1deb03
kyrylo <kirillulianow@gmail.com>
2024-05-21 13:23:33 +0300

    added api func for refresh endpoint

--- src/core/api/auth/auth.js
+++ src/core/api/auth/auth.js
@@ -1,4 +1,11 @@
-import { loginUrl, logoutUrl, registerUrl } from 'src/core/config/endpoints';
+import axios from 'axios';
+
+import {
+    loginUrl,
+    logoutUrl,
+    refreshTokenUrl,
+    registerUrl,
+} from 'src/core/config/endpoints';
 import { $api } from 'src/core/http';
 
 export const signupRequest = async (username, email, password) => {
@@ -22,6 +29,17 @@ export const loginRequest = async (email, password) => {
 export const logoutRequest = async () => {
     try {
         const response = await $api.get(logoutUrl);
+        return response.data;
+    } catch (error) {
+        throw error.message;
+    }
+};
+
+export const refreshTokenRequest = async () => {
+    try {
+        const response = await axios.get(refreshTokenUrl, {
+            withCredentials: true,
+        });
         return response.data;
     } catch (error) {
         throw error.message;
