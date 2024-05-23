8b130d68a48ea0a77f177924e41d4b169232ac2f
Whitegolyb <kirillulianow@gmail.com>
2024-05-23 20:42:24 +0300

    changed validation

--- src/core/shemes/CalculatorValidation.jsx
+++ src/core/shemes/CalculatorValidation.jsx
@@ -1,31 +1,75 @@
-import * as Yup from 'yup';
+export const validationSchema = {
+    distance: {
+        required: true,
+        validate: {
+            number: value => !isNaN(value) || 'Значення повинно бути числовим',
+            positive: value => parseFloat(value) > 0 || 'Число повинно бути більше 0',
+        },
+    },
 
-export const validationSchema = Yup.object({
-    distance: Yup.number()
-        .positive("Значення не повинно бути від'ємним")
-        .typeError("Обов'язкове числове поле!"),
-    spotHeight: Yup.number()
-        .positive("Значення не повинно бути від'ємним")
-        .typeError("Обов'язкове числове поле!"),
-    spotWidth: Yup.number()
-        .positive("Значення не повинно бути від'ємним")
-        .typeError("Обов'язкове числове поле!"),
-    angleWidth: Yup.number()
-        .positive("Значення не повинно бути від'ємним")
-        .typeError("Обов'язкове числове поле!"),
-    angleHeight: Yup.number()
-        .positive("Значення не повинно бути від'ємним")
-        .typeError("Обов'язкове числове поле!"),
-    sensitivity: Yup.number()
-        .positive("Значення не повинно бути від'ємним")
-        .typeError("Обов'язкове числове поле!"),
-    power: Yup.number()
-        .positive("Значення не повинно бути від'ємним")
-        .typeError("Обов'язкове числове поле!"),
-    minPlumeSize: Yup.number()
-        .positive("Значення не повинно бути від'ємним")
-        .typeError("Обов'язкове числове поле!"),
-    distanceModuleThird: Yup.number()
-        .positive("Значення не повинно бути від'ємним")
-        .typeError("Обов'язкове числове поле!"),
-});
+    spotHeight: {
+        required: true,
+        validate: {
+            number: value => !isNaN(value) || 'Значення повинно бути числовим',
+            positive: value => parseFloat(value) > 0 || 'Число повинно бути більше 0',
+        },
+    },
+
+    spotWidth: {
+        required: true,
+        validate: {
+            number: value => !isNaN(value) || 'Значення повинно бути числовим',
+            positive: value => parseFloat(value) > 0 || 'Число повинно бути більше 0',
+        },
+    },
+
+    angleWidth: {
+        required: true,
+        validate: {
+            number: value => !isNaN(value) || 'Значення повинно бути числовим',
+            moreThan180: value =>
+                parseFloat(value) <= 180 || 'Кут не може бути 180° та більше',
+            positive: value => parseFloat(value) > 0 || 'Кут не може бути 0° та менше',
+        },
+    },
+
+    angleHeight: {
+        required: true,
+        validate: {
+            number: value => !isNaN(value) || 'Значення повинно бути числовим',
+            moreThan180: value =>
+                parseFloat(value) <= 180 || 'Кут не може бути 180° та більше',
+            positive: value => parseFloat(value) > 0 || 'Кут не може бути 0° та менше',
+        },
+    },
+
+    sensitivity: {
+        required: true,
+        validate: {
+            number: value => !isNaN(value) || 'Значення повинно бути числовим',
+            positive: value => parseFloat(value) > 0 || 'Число повинно бути більше 0',
+        },
+    },
+
+    power: {
+        required: true,
+        validate: {
+            number: value => !isNaN(value) || 'Значення повинно бути числовим',
+            positive: value => parseFloat(value) > 0 || 'Число повинно бути більше 0',
+        },
+    },
+
+    minPlumeSize: {
+        validate: {
+            number: value => !isNaN(value) || 'Значення повинно бути числовим',
+            positive: value => parseFloat(value) >= 0 || 'Число повинно бути позитивним',
+        },
+    },
+
+    distanceModuleThird: {
+        validate: {
+            number: value => !isNaN(value) || 'Значення повинно бути числовим',
+            positive: value => parseFloat(value) >= 0 || 'Число повинно бути позитивним',
+        },
+    },
+};
