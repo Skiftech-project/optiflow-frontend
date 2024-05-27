2279f982f41962e6b88a000c47dabc9e4cbb0253
kyrylo <kirillulianow@gmail.com>
2024-05-27 15:16:30 +0300

    corrected page styles

--- src/components/pages/RayPage.jsx
+++ src/components/pages/RayPage.jsx
@@ -1,14 +1,35 @@
-import { Box } from '@mui/material';
+import { useState } from 'react';
+
+import { Box, IconButton } from '@mui/material';
+
+import KeyboardDoubleArrowRightIcon from '@mui/icons-material/KeyboardDoubleArrowRight';
 
 import { RayCanvas } from '../canvas';
 import { Header } from '../interface';
 import { ErrorBoundary } from '../ui';
 
 const RayPage = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const toggleMenu = state => () => {
+        setMenuOpen(state);
+    };
+    console.log(menuOpen);
+
     return (
         <>
             <Header position="fixed" />
-            <Box component="main" height="100vh">
+            <Box
+                component="main"
+                sx={{
+                    height: `calc(100vh - 64px)`,
+                    paddingTop: '64px',
+                }}
+            >
+                <IconButton onClick={toggleMenu(true)}>
+                    <KeyboardDoubleArrowRightIcon fontSize="large" />
+                </IconButton>
+
                 <ErrorBoundary>
                     <RayCanvas />
                 </ErrorBoundary>
