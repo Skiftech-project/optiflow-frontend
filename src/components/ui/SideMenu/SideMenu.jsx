4a4739ad4cbb5dadd57ac0081fe7ea8471e6e640
Whitegolyb <kirillulianow@gmail.com>
2024-05-28 12:03:02 +0300

    adde side menu

---
+++ src/components/ui/SideMenu/SideMenu.jsx
@@ -0,0 +1,50 @@
+import PropTypes from 'prop-types';
+
+import { Divider, IconButton, Paper, Stack, Typography } from '@mui/material';
+
+import KeyboardDoubleArrowLeftIcon from '@mui/icons-material/KeyboardDoubleArrowLeft';
+
+const menuStyles = {
+    width: 400,
+    height: '100%',
+    position: 'absolute',
+    zIndex: '5',
+    transition: '0.5s',
+    p: 2,
+};
+
+const SideMenu = ({ open = false, onClose = null, children }) => {
+    return (
+        <Paper
+            variant="outlined"
+            sx={{
+                ...menuStyles,
+                transform: open ? 'translateX(0)' : 'translateX(-100%)',
+            }}
+        >
+            <Stack direction="row" alignItems="center" justifyContent="space-between">
+                <Typography
+                    color="primary"
+                    variant="overline"
+                    fontWeight={700}
+                    fontSize={15}
+                >
+                    Перебудувати промінь
+                </Typography>
+                <IconButton onClick={onClose} size="small" edge="start">
+                    <KeyboardDoubleArrowLeftIcon fontSize="large" />
+                </IconButton>
+            </Stack>
+            <Divider sx={{ my: 1 }} />
+            {children}
+        </Paper>
+    );
+};
+
+SideMenu.propTypes = {
+    open: PropTypes.bool,
+    onClose: PropTypes.func,
+    children: PropTypes.node,
+};
+
+export default SideMenu;
