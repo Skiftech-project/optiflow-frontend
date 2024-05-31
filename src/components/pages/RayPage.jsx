fad61f64b624117e27c7c34d1a6832e2413bfda4
kyrylo <kirillulianow@gmail.com>
2024-05-31 15:06:32 +0300

    ray page corrections

--- src/components/pages/RayPage.jsx
+++ src/components/pages/RayPage.jsx
@@ -5,11 +5,12 @@ import { Box, IconButton, Tooltip } from '@mui/material';
 import KeyboardDoubleArrowRightIcon from '@mui/icons-material/KeyboardDoubleArrowRight';
 
 import { RayCanvas } from '../canvas';
-import { Header } from '../interface';
+import { Header, RayForm } from '../interface';
 import { ErrorBoundary, SideMenu } from '../ui';
 
 const RayPage = () => {
     const [menuOpen, setMenuOpen] = useState(false);
+    const [calcOption, setCalcOption] = useState(0);
 
     const toggleMenu = state => () => {
         setMenuOpen(state);
@@ -22,11 +23,13 @@ const RayPage = () => {
                 component="main"
                 sx={{
                     height: `calc(100vh - 64px)`,
-                    paddingTop: '64px',
+                    marginTop: '64px',
                     position: 'relative',
                 }}
             >
-                <SideMenu open={menuOpen} onClose={toggleMenu(false)}></SideMenu>
+                <SideMenu open={menuOpen} onClose={toggleMenu(false)}>
+                    <RayForm calcOption={calcOption} toggleCalcOption={setCalcOption} />
+                </SideMenu>
 
                 <Tooltip
                     title="Відкрити меню для розрахування променю "
