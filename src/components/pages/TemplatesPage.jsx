5cf334b09273dfda80a4e66431efebfacd35f63c
VladSurkov <surkov.vladyslav.34@gmail.com>
2024-05-31 13:38:26 +0300

    changed template block title

--- src/components/pages/TemplatesPage.jsx
+++ src/components/pages/TemplatesPage.jsx
@@ -8,9 +8,9 @@ const TemplatesPage = () => {
             <Header sx={{ marginBottom: '65px' }} />
 
             <Stack direction="column" gap={5} alignItems="center">
-                <TemplateBlock title="Fuck Putin"></TemplateBlock>
+                <TemplateBlock title="Test 1"></TemplateBlock>
 
-                <TemplateBlock title="GG"></TemplateBlock>
+                <TemplateBlock title="Test 2"></TemplateBlock>
             </Stack>
         </div>
     );
