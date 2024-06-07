59a6cc48985a62665da13790afc2617577f3b67a
Whitegolyb <kirillulianow@gmail.com>
2024-06-07 14:12:35 +0300

    added spinner for templates loading process

--- src/components/pages/TemplatesPage.jsx
+++ src/components/pages/TemplatesPage.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import { useEffect } from 'react';
 import { useSelector } from 'react-redux';
+import { HashLoader } from 'react-spinners';
 
 import { Stack, Typography } from '@mui/material';
 
@@ -9,7 +10,9 @@ import { useTemplateService } from 'src/core/services';
 import { Header, TemplateBlock } from '../interface';
 
 const TemplatesPage = () => {
-    const { templates, errorMessage } = useSelector(state => state.getTemplates);
+    const { templates, errorMessage, templatesLoadingStatus } = useSelector(
+        state => state.getTemplates,
+    );
 
     const { getAllTemplates } = useTemplateService();
 
@@ -23,6 +26,8 @@ const TemplatesPage = () => {
             <Stack direction="column" gap={5} alignItems="center">
                 {errorMessage ? (
                     <Typography>{errorMessage}</Typography>
+                ) : templatesLoadingStatus === 'loading' ? (
+                    <HashLoader color="#1e55b3" />
                 ) : (
                     templates.map(item => {
                         return (
