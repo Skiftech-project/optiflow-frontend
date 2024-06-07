73d03403b6ee65f376b47d8e0893004267cc7bad
Whitegolyb <kirillulianow@gmail.com>
2024-06-07 18:58:09 +0300

    removed ErrorBoundary

--- src/components/pages/RayPage.jsx
+++ src/components/pages/RayPage.jsx
@@ -2,16 +2,13 @@ import { Box } from '@mui/material';
 
 import { RayCanvas } from '../canvas';
 import { Header } from '../interface';
-import { ErrorBoundary } from '../ui';
 
 const RayPage = () => {
     return (
         <>
             <Header position="fixed" />
             <Box component="main" height="100vh">
-                <ErrorBoundary>
-                    <RayCanvas />
-                </ErrorBoundary>
+                <RayCanvas />
             </Box>
         </>
     );
